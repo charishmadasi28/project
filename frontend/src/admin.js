b79d70b5282f91b6f0bec84565c45e7ccbaa2734
charishmadasi28 <dasicharishma@gmail.com>
2023-09-04 21:25:14 +0530

    hi

--- frontend/src/admin.js
+++ frontend/src/admin.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { useNavigate } from "react-router-dom";
 
 const Adminlogin = () => {
+    const nav=useNavigate();
   const [mail, setmail] = useState('');
   const [password, setpassword] = useState('');
 
   const Submit=async() =>{
     try{
-     const result = await axios.post("http://localhost:8000/admin/"+mail+'/'+password);
+     const result = await axios.post("https://server-vqm4.onrender.com/admin/"+mail+'/'+password);
      console.log(result.data)
         if(result.data)
         {
